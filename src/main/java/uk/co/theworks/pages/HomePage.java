d9523879303d110bc771499275dd256fef97318f
imcaptaingit <imcaptaingit>
2023-08-30 20:43:36 +0100

    update 1

---
+++ src/main/java/uk/co/theworks/pages/HomePage.java
@@ -0,0 +1,8 @@
+package uk.co.theworks.pages;
+
+/**
+ * Author - Sarvat Shaikh
+ * Project Name: The-Works-B2S
+ */
+public class HomePage {
+}
---
+++ src/test/java/uk/co/theworks/testsuite/HomePageTest.java
@@ -0,0 +1,8 @@
+package uk.co.theworks.testsuite;
+
+/**
+ * Author - Sarvat Shaikh
+ * Project Name: The-Works-B2S
+ */
+public class HomePageTest {
+}
