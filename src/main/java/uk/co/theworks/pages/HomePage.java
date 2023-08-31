d2534549781da6b562c8c139f6e3a406b9d749d8
jack138138 <jackpeter138138@gmail.com>
2023-08-31 01:00:31 +0100

    update by jd

--- src/main/java/uk/co/theworks/pages/HomePage.java
+++ src/main/java/uk/co/theworks/pages/HomePage.java
@@ -1,8 +1,27 @@
 package uk.co.theworks.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import uk.co.theworks.utility.Utility;
+
 /**
- * Author - Sarvat Shaikh
+ * Author - Jaydeep Patel
  * Project Name: The-Works-B2S
  */
-public class HomePage {
+public class HomePage extends Utility {
+
+    @CacheLookup
+    @FindBy (xpath = "//header/nav[1]/div[1]/div[3]/nav[1]/ul[1]/li[2]/a[1]")
+    WebElement kids;
+
+
+
+    public void clickOnKidsLink(){
+        clickOnElement(kids);
+    }
+
+
+
+
 }
